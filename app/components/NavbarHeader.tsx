9f0473052c67c255faed9ded9ce35729011f7bd0
Ronny019 <mainak.pc@gmail.com>
2025-08-01 19:58:02 -0700

    Navbar cluster name changes

--- app/components/NavbarHeader.tsx
+++ app/components/NavbarHeader.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import LogoNexus from "./LogoNexus";
+import ChevronDownIcon from "./ChevronDownIcon";
 import { useCluster } from "../context/ClusterContext"; // adjust path as needed
 
 type Cluster = {
@@ -29,23 +30,36 @@ export default function NavbarHeader() {
 
   return (
     <div className="flex items-center">
-      <LogoNexus width={50} height={50} className="mr-1" /> {/* Changed from mr-2 to mr-1 */}
-      <select
-        className="text-white outline-none font-nunito text-lg h-[50px] px-2"
-        value={selectedCluster}
-        onChange={e => setSelectedCluster(e.target.value)}
-      >
-        {clusters.map(cluster => (
-          <option
-            key={cluster.cluster_id}
-            value={cluster.cluster_id}
-            className="text-black font-nunito text-lg"
-            style={{ backgroundColor: "#242C35", color: "#fff" }}
-          >
-            {cluster.cluster_name}
-          </option>
-        ))}
-      </select>
+      <LogoNexus width={50} height={50} className="mr-1" />
+      <div className="relative flex items-center">
+        <select
+          className="text-white outline-none font-nunito text-lg h-[50px] px-2 pr-8 bg-[#242C35]"
+          value={selectedCluster}
+          onChange={e => setSelectedCluster(e.target.value)}
+          style={{
+            backgroundColor: "#242C35",
+            color: "#fff",
+            WebkitAppearance: "none",
+            MozAppearance: "none",
+            appearance: "none",
+          }}
+        >
+          {clusters.map(cluster => (
+            <option
+              key={cluster.cluster_id}
+              value={cluster.cluster_id}
+              className="text-black font-nunito text-lg"
+              style={{
+                backgroundColor: "#23272f",
+                color: "#fff",
+              }}
+            >
+              {cluster.cluster_name}
+            </option>
+          ))}
+        </select>
+        <ChevronDownIcon className="pointer-events-none absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
+      </div>
     </div>
   );
 }
